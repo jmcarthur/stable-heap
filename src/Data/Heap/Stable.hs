423a726020df8bc4fb80ac4b68b3df47faf99b2d
Jake McArthur <Jake.McArthur@gmail.com>
2015-05-16 22:12:22 +0000

    some instances

--- src/Data/Heap/Stable.hs
+++ src/Data/Heap/Stable.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
 module Data.Heap.Stable (Heap (), empty, singleton, union, splitMin) where
 
 import Data.Monoid
@@ -5,6 +8,7 @@ import Data.Monoid
 data Heap k a
   = Heap !(Heap k a) (Heap k a) !k a (Heap k a) !(Heap k a)
   | Empty
+  deriving (Functor, Foldable, Traversable)
 
 empty :: Heap k a
 empty = Empty
@@ -28,3 +32,7 @@ xs@(Heap l1 ls1 k1 v1 rs1 r1) `union` ys@(Heap l2 ls2 k2 v2 rs2 r2)
 splitMin :: Ord k => Heap k a -> Maybe (Heap k a, k, a, Heap k a)
 splitMin Empty = Nothing
 splitMin (Heap l ls k v rs r) = Just (l `union` ls, k, v, rs `union` r)
+
+instance Ord k => Monoid (Heap k a) where
+  mempty = empty
+  mappend = union
