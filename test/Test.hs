5344691c5e80ec109dda5c6532af636037ebc677
Jake McArthur <Jake.McArthur@gmail.com>
2023-08-13 15:11:10 -0400

    another attempt to fix the tests under 7.8

--- test/Test.hs
+++ test/Test.hs
@@ -6,10 +6,10 @@ import Control.Applicative
 import Control.Monad.Trans.Writer
 import Data.Heap.Stable (Heap)
 import qualified Data.Heap.Stable as Heap
-import Data.Foldable
+import Data.Foldable (foldMap)
 import Data.List
 import Data.Ord
-import Data.Traversable
+import Data.Traversable (traverse)
 import Data.Tuple
 import Test.QuickCheck.Function
 import Test.Tasty
